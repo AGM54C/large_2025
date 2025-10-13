a56fb0798dd1e992773119b3e455c8268321f16b
agm54c <agm54c@gmail.com>
2025-10-13 19:32:04 +0800

    创建预训练模型表格

--- src/main/java/com/example/large_2025/Large2025Application.java
+++ src/main/java/com/example/large_2025/Large2025Application.java
@@ -2,12 +2,14 @@ package com.example.large_2025;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.example.large_2025.pojo")  // 明确指定实体类包
+@EnableJpaRepositories("com.example.large_2025.repository")  // 如果有repository
 public class Large2025Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Large2025Application.class, args);
     }
-
 }
